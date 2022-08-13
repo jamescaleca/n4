590cabd2fdc084ff50d86157a7745f683f8c46bb
James Caleca <jacaleca2@gmail.com>
2022-08-13 12:52:34 -0600

    Styling changes to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,22 +1,58 @@
+import Image from 'next/image'
+
 const Footer = () => {
   return (
-    <footer className="bg-gray-50 border-t py-12 h-32">
+    <footer className="py-8">
       <div className="max-w-5xl mx-auto flex justify-between">
-        <div className="flex items-center space-x-4">
-          <img
-            src="/react-bricks-icon.svg"
-            alt="React Bricks"
-            className="w-8"
+        <div className="flex flex-col items-center justify-between h-28">
+          <Image
+            src="/images/Logo/Black/N4-Enterprises-Black.png"
+            alt="N4 Logo"
+            width={120}
+            height={70}
           />
-          <div className="uppercase text-sm tracking-wider">
-            Next.js starter
-          </div>
+          <ul className="flex flex-row justify-between w-36">
+            <li>
+              <img
+                src="/images/Icons/facebook-brands.svg"
+                alt="FB"
+                width={20}
+                height={20}
+                className="svg"
+              />
+            </li>
+            <li>
+              <img
+                src="/images/Icons/instagram-brands.svg"
+                alt="Insta"
+                width={20}
+                height={20}
+                className="svg"
+              />
+            </li>
+            <li>
+              <img
+                src="/images/Icons/twitter-brands.svg"
+                alt="Twitter"
+                width={20}
+                height={20}
+                className="svg"
+              />
+            </li>
+            <li>
+              <img
+                src="/images/Icons/youtube-brands.svg"
+                alt="YT"
+                width={20}
+                height={20}
+                className="svg"
+              />
+            </li>
+          </ul>
         </div>
-        <div className="text-gray-500 text-sm">
+        <div className="text-sm">
           © {new Date().getFullYear()}{' '}
-          <a href="https://reactbricks.com" className="hover:text-pink-600">
-            React Bricks
-          </a>
+            N4 Enterprises
         </div>
       </div>
     </footer>
